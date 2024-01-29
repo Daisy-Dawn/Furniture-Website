84e1f888e641a28e9989514b4dfd427934cdf268
Daisy-Dawn <agboella20@gmail.com>
2024-01-29 01:15:53 +0100

    removed Firebase package and updated app

--- src/pages/UserAuth/ForgotPassword1.jsx
+++ src/pages/UserAuth/ForgotPassword1.jsx
@@ -23,17 +23,17 @@ const ForgotPassword1 = () => {
   }
 
   return (
-    <div className=' grid mx-[8rem] md:grid-cols-2 grid-cols-1  mb-0 font-nunito bg-white'>
-        <div className='h-screen w-full'>
+    <div className='h-screen lg:h-full grid mx-[1rem] lg:mx-[8rem] md:grid-cols-2 grid-cols-1  mb-0 font-nunito bg-white'>
+        <div className='h-screen w-full hidden md:block'>
           <img className='h-full w-full object-cover' src={forgotPassword} alt="" />
         </div>
 
         <form className='flex justify-center gap-[3rem] flex-col' onSubmit={onFormSubmission} action="">
-          <h2 className='text-[3rem] font-extrabold text-violet-900'> Reset Password? </h2>
-          <p className='text-violet-900 text-[1.6rem] font-semibold'> Enter the email address associated with your account and we will send you an email with confirmation to reset your password. </p>
+          <h2 className='lg:text-[3rem] text-[2.4rem] font-extrabold text-violet-900'> Reset Password? </h2>
+          <p className='text-violet-900 lg:text-[1.6rem] text-[1.15rem] font-semibold'> Enter the email address associated with your account and we will send you an email with confirmation to reset your password. </p>
 
           <div>
-          <input  placeholder='Enter Email Address' className='w-full text-violet-900 lg:p-[1rem] placeholder:text-[1.5rem] p-[0.4rem] text-[1.8rem] lg:mb-2 mb-0 border-violet-700
+          <input  placeholder='Enter Email Address' className='w-full text-violet-900 lg:p-[1rem] lg:placeholder:text-[1.5rem] placeholder:text-[1rem] p-[0.4rem] text-[1.2rem] lg:text-[1.8rem] lg:mb-2 mb-0 border-violet-700
            placeholder:text-violet-400 border-b-[3px] outline-none' type="email" value={email} onChange={(e) => {
             setEmail(e.target.value)
             setError("")
@@ -45,7 +45,7 @@ const ForgotPassword1 = () => {
            
         
           <div className='justify-end flex w-full'>
-          <button type='submit' className='bg-violet-900 text-white text-[1.5rem] font-semibold rounded-xl px-[3rem] flex justify-center items-center py-[1rem]'>
+          <button type='submit' className='bg-violet-900 text-white text-[1.2rem] lg:text-[1.5rem] font-semibold rounded-xl px-[2.2rem] lg:px-[3rem] flex justify-center items-center py-[0.75rem] lg:py-[1rem]'>
             Next
           </button>
           </div>
